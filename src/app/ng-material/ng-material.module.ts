c248c2fad317a0a7d98a8662e69ca86129729471
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2020-02-09 15:20:39 +0530

    Updated to Angular 9.0

--- src/app/ng-material/ng-material.module.ts
+++ src/app/ng-material/ng-material.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  MatButtonModule, MatCardModule, MatInputModule, MatIconModule,
-  MatDividerModule, MatProgressSpinnerModule, MatToolbarModule,
-  MatSelectModule, MatMenuModule, MatSnackBarModule, MatTooltipModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
