9f83985cc3431f7a9155411956382ecd98acd322
AnkitSharma-007 <ankit.1si10cs010@gmail.com>
2021-11-22 20:42:27 +0530

    angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { environment } from '../environments/environment';
-import { RouterModule } from '@angular/router';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { ShareButtonsConfig, ShareModule } from '@ngx-share/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HttpClientModule } from '@angular/common/http';
-import { NgMaterialModule } from './ng-material/ng-material.module';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { BlogComponent } from './components/blog/blog.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { HomeComponent } from './components/home/home.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { ScrollerComponent } from './components/scroller/scroller.component';
-import { BlogEditorComponent } from './components/blog-editor/blog-editor.component';
-import { BlogCardComponent } from './components/blog-card/blog-card.component';
-import { ExcerptPipe } from './customPipes/excerpt.pipe';
-import { SlugPipe } from './customPipes/slug.pipe';
-import { SocialShareComponent } from './components/social-share/social-share.component';
-import { PaginatorComponent } from './components/paginator/paginator.component';
-import { AuthorProfileComponent } from './components/author-profile/author-profile.component';
-import { AuthGuard } from './guards/auth.guard';
-import { AdminAuthGuard } from './guards/admin-auth.guard';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { environment } from "../environments/environment";
+import { RouterModule } from "@angular/router";
+import { CKEditorModule } from "@ckeditor/ckeditor5-angular";
+import { FormsModule } from "@angular/forms";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { HttpClientModule } from "@angular/common/http";
+import { NgMaterialModule } from "./ng-material/ng-material.module";
+import { NgxPaginationModule } from "ngx-pagination";
+import { BlogComponent } from "./components/blog/blog.component";
+import { CommentsComponent } from "./components/comments/comments.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { ScrollerComponent } from "./components/scroller/scroller.component";
+import { BlogEditorComponent } from "./components/blog-editor/blog-editor.component";
+import { BlogCardComponent } from "./components/blog-card/blog-card.component";
+import { ExcerptPipe } from "./customPipes/excerpt.pipe";
+import { SlugPipe } from "./customPipes/slug.pipe";
+import { SocialShareComponent } from "./components/social-share/social-share.component";
+import { PaginatorComponent } from "./components/paginator/paginator.component";
+import { AuthorProfileComponent } from "./components/author-profile/author-profile.component";
+import { AuthGuard } from "./guards/auth.guard";
+import { AdminAuthGuard } from "./guards/admin-auth.guard";
+import { ShareIconsModule } from "ngx-sharebuttons/icons";
+import { ShareButtonsConfig, ShareModule } from "ngx-sharebuttons";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 const customConfig: ShareButtonsConfig = {
-  twitterAccount: 'ankitsharma_007'
+  autoSetMeta: true,
+  twitterAccount: "ankitsharma_007",
 };
 
 @NgModule({
@@ -50,6 +52,7 @@ const customConfig: ShareButtonsConfig = {
     AuthorProfileComponent,
   ],
   imports: [
+    ShareIconsModule,
     NgxPaginationModule,
     HttpClientModule,
     FontAwesomeModule,
@@ -62,16 +65,26 @@ const customConfig: ShareButtonsConfig = {
     NgMaterialModule,
     CKEditorModule,
     FormsModule,
-    RouterModule.forRoot([
-    { path: '', component: HomeComponent, pathMatch: 'full' },
-    { path: 'page/:pagenum', component: HomeComponent },
-    { path: 'addpost', component: BlogEditorComponent, canActivate: [AuthGuard] },
-    { path: 'editpost/:id', component: BlogEditorComponent, canActivate: [AdminAuthGuard] },
-    { path: 'blog/:id/:slug', component: BlogComponent },
-    { path: '**', component: HomeComponent }
-], { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(
+      [
+        { path: "", component: HomeComponent, pathMatch: "full" },
+        { path: "page/:pagenum", component: HomeComponent },
+        {
+          path: "addpost",
+          component: BlogEditorComponent,
+          canActivate: [AuthGuard],
+        },
+        {
+          path: "editpost/:id",
+          component: BlogEditorComponent,
+          canActivate: [AdminAuthGuard],
+        },
+        { path: "blog/:id/:slug", component: BlogComponent },
+        { path: "**", component: HomeComponent },
+      ],
+      { relativeLinkResolution: "legacy" }
+    ),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/social-share/social-share.component.ts
+++ src/app/components/social-share/social-share.component.ts
@@ -1,16 +1,11 @@
-import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { iconpack } from 'src/icons';
-import { ShareService } from '@ngx-share/core';
+import { Component } from "@angular/core";
+import { ShareService } from "ngx-sharebuttons";
 
 @Component({
-  selector: 'app-social-share',
-  templateUrl: './social-share.component.html',
-  styleUrls: ['./social-share.component.scss']
+  selector: "app-social-share",
+  templateUrl: "./social-share.component.html",
+  styleUrls: ["./social-share.component.scss"],
 })
 export class SocialShareComponent {
-
-  constructor(library: FaIconLibrary, public share: ShareService) {
-    library.addIcons(...iconpack);
-  }
+  constructor(public share: ShareService) {}
 }
